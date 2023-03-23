8fa4caefbdd95035c27974efef2435843d358f2d
Sam <samspeciale1997@gmail.com>
2023-03-23 17:17:15 -0500

    first commmit

---
+++ Coursework/Vault/script.js
@@ -0,0 +1,10 @@
+/*
+First number will be 85 - 75 = 10
+Second number will be 400 / 10 = 40
+Third number will be 13 + 26 = 39
+*/
+alert('You have received this message because you have been chosen to open an important vault. Here is the secret combination');
+let first = (85 - 75);
+let second = (400 / 10)
+let third = (13 + 26)
+alert(first + "-" + second + "-" + third)
